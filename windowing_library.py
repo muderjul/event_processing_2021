602a4b19ee7ad22f3f48f958e3ec21572ea87e24
Julian Muders <julian.muders@deliveryhero.com>
2021-06-29 17:33:27 +0200

    added timing for windowing_library

--- windowing_library.py
+++ windowing_library.py
@@ -1,7 +1,9 @@
 from csv import DictReader
 
 from event import Event
-from ReactiveAggregator import ReactiveAggregator
+from ReactiveAggregator_static import ReactiveAggregatorStatic as ReactiveAggregator
+
+from time import perf_counter
 
 
 def read_events_from_file(dataset_name: str, max_events: int):
@@ -24,6 +26,7 @@ def read_events_from_file(dataset_name: str, max_events: int):
 
 def simulate_window(dataset_name: str, reactive_aggregator: ReactiveAggregator, max_events: int = None, clock_interval: int = 500, window_size: int = 5000):
     window = []
+    time = 0
     with open(f"dataset/{dataset_name}.csv", 'r') as csv_file:
         csv_reader = DictReader(csv_file)
         window_begin = - window_size
@@ -45,14 +48,18 @@ def simulate_window(dataset_name: str, reactive_aggregator: ReactiveAggregator, 
                     # evict event_in_window!
                     evict_ids.append(event_id)
             for evict_id in evict_ids:
+                start = perf_counter()
                 reactive_aggregator.evict([window[evict_id]])
+                time += perf_counter() - start
             window = [event_in_window for event_idx, event_in_window in enumerate(window) if event_idx not in evict_ids]
 
             replaced = False
             # B. check for replacements (TRIGGER) before
             for event_id, event_in_window in enumerate(window):
                 if event_in_window.key == event.key:
+                    start = perf_counter()
                     reactive_aggregator.trigger([event])
+                    time += perf_counter() - start
                     window = [event_in_window for event_idx, event_in_window in enumerate(window) if event_idx != event_id]
                     window.append(event)
                     replaced = True
@@ -60,10 +67,13 @@ def simulate_window(dataset_name: str, reactive_aggregator: ReactiveAggregator, 
 
             # C. else: INSERT the new event
             if not replaced:
+                start = perf_counter()
                 reactive_aggregator.insert([event])
+                time += perf_counter() - start
                 window.append(event)
 
             if max_events is not None and i + 1 >= max_events:
                 break
 
-            print(reactive_aggregator.submit())
+            #print(reactive_aggregator.submit())
+    print(time)
